bfe68a8e7b58664d343cb963633b66d4dc2c2179
n-d-r <crtl.ltrc.privat@googlemail.com>
2016-09-08 21:54:26 +0200

    added descriptive analysis of features; and cov/cor matrices

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -2,6 +2,7 @@
 # Setup
 #===============================================================================
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,7 +13,10 @@ from sklearn.decomposition import PCA
 #===============================================================================
 
 data = pd.read_csv('ionosphere_processed.csv', index_col=0)
-X = data.as_matrix()[:, :data.shape[1]-1]
+data_mat = data.as_matrix()
+n_rows, n_cols = data_mat.shape
+X = data_mat[:, :n_cols-1]
+y = data_mat[:, n_cols-1]
 
 #===============================================================================
 # Function definitions
@@ -30,6 +34,35 @@ def scree_plot(explained_var, save=False):
   else:
     plt.savefig('scree_plot.png', bbox_inches='tight')
     plt.close('all')
+
+#===============================================================================
+# Summary statistics of the features
+#===============================================================================
+
+data_descript = data.describe()
+# as can be seen, one of the features equals 
+# zero in all observations, so it can be removed
+to_remove_cols = data_descript.columns[data_descript.loc['mean', :] == 0]
+data_filt = data.loc[
+  :, [col for col in data.columns if col not in to_remove_cols]
+]
+X = data_filt.as_matrix()[:, :data_filt.shape[1]-1]
+
+#===============================================================================
+# Covariance and correlation matrices of the features
+#===============================================================================
+
+X_centred = X
+for i in range(X_centred.shape[1]):
+  X_centred[:, i] = X_centred[:, i] - np.mean(X_centred[:, i])
+
+cov_mat = (X.T).dot(X) / (X_centred.shape[0]-1)
+diagonals = np.diag(cov_mat).astype(float)
+diag_sqrt = np.sqrt(diagonals)
+diag_sqrt_inv = 1/diag_sqrt
+D_inv = np.zeros((len(diag_sqrt), len(diag_sqrt)))
+np.fill_diagonal(D_inv, diag_sqrt_inv)
+cor_mat = D_inv.dot(cov_mat).dot(D_inv)
 
 #===============================================================================
 # Principal component analysis of the features
